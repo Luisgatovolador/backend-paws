585c2101f79525ac1ebe0099501c866e970684fb
CasillasCesar <cesarcasillascespedes@gmail.com>
2025-09-22 02:11:22 -0600

    feature : Se genera un documento para validar parametros enviados para obtener la geolocalizacion.

---
+++ src/validators/locationValidators.js
@@ -0,0 +1,21 @@
+'use strict'
+const Joi = require('joi');
+
+const locationSchema = Joi.object({
+  userId: Joi.number()
+    .required()
+    .messages({
+      'any.required': 'El ID de usuario es requerido.',
+      'number.base': 'El ID de usuario debe ser un número.'
+    }),
+  actionName: Joi.string()
+    .required()
+    .messages({
+      'any.required': 'El tipo de acción es requerido.',
+      'string.base': 'El tipo de acción debe ser una cadena de texto.'
+    })
+});
+
+module.exports = {
+  locationSchema
+};
