f8b44bd844e9d0c9f5b2ec086b19f1fb94d2d9fa
CasillasCesar <cesarcasillascespedes@gmail.com>
2025-09-22 02:46:36 -0600

    fix : Cambio de metodo

--- src/routes/location.js
+++ src/routes/location.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const locationController = require('../controllers/locationController');
 
 // Ruta para guardar la ubicación a través de la IP
-router.get('/get-by-id', locationController.getLocationsByUserId);
+router.post('/get-by-id', locationController.getLocationsByUserId);
 
 module.exports = router;
